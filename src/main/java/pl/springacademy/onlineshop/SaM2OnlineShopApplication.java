1fed92b15700d01a48416ed03d8d98636c9f609b
pawel.bachta <pawel.bachta@gmail.com>
2022-01-10 20:14:05 +0100

    init commit

---
+++ src/main/java/pl/springacademy/onlineshop/SaM2OnlineShopApplication.java
@@ -0,0 +1,12 @@
+package pl.springacademy.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaM2OnlineShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaM2OnlineShopApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/springacademy/onlineshop/SaM2OnlineShopApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.springacademy.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaM2OnlineShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
